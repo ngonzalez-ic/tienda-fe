272a6ffbbfa24c7cc357d89ca3b0f049ea23f0af
unknown <cgonzalez@iiconcepcion.edu.ar>
2021-01-07 00:46:07 -0300

    sale componente ok

--- src/components/sale/saleProduct.js
+++ src/components/sale/saleProduct.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { getProductsbyID } from '../../utils'
 import Loading from '../loading/Loading'
-import { Card, Media, Image, Content, Heading } from 'react-bulma-components'
-
+import { Card, Media, Image, Content, Heading, Columns, Box, Section, Level, Button } from 'react-bulma-components'
 const SaleProduct = (props) => {
   console.log(props.match.params.id)
   const key = props.match.params.id
@@ -22,30 +21,49 @@ const SaleProduct = (props) => {
   return (
     <>
       {isLoading && <Loading />}
-      {!isLoading && <Card style={{ display: 'flex', justifyContent: 'center' }}>
-        <Card.Header>
-          <Card.Header.Title>{producto.name}</Card.Header.Title>
-        </Card.Header>
-        <Card.Content>
-          <Media>
-            <Media.Item renderAs='figure' position='left'>
-              <Image size={64} alt='64x64' src='http://bulma.io/images/placeholders/128x128.png' />
-            </Media.Item>
-            <Media.Item>
-              <Heading size={4}>John Smith</Heading>
-              <Heading subtitle size={6}>@johnsmith</Heading>
-            </Media.Item>
-          </Media>
-          <Content>
-            {producto.description}
-          </Content>
-        </Card.Content>
-        <Card.Footer>
-          <Card.Footer.Item renderAs='a' href='#Yes'>Yes</Card.Footer.Item>
-          <Card.Footer.Item renderAs='a' href='#No'>No</Card.Footer.Item>
-          <Card.Footer.Item renderAs='a' href='#Maybe'>Maybe</Card.Footer.Item>
-        </Card.Footer>
-      </Card>}
+      {!isLoading && <Columns>
+        <Columns.Column size='half' offset='one-quarter'>
+          <Card>
+            <Card.Header>
+              <Card.Header.Title>{producto.name}</Card.Header.Title>
+            </Card.Header>
+            <Card.Content>
+              <Media>
+                <Card.Image size='4by3' style={{ width: 300, height: 400, display: 'flex', justifyContent: 'center' }} src={producto.image_path} />
+                <Media.Item>
+                  <Content style={{ display: 'flex', justifyContent: 'center', alineItem: 'center' }}>
+                    {producto.description}
+                  </Content>
+                </Media.Item>
+              </Media>
+
+            </Card.Content>
+            <Card.Footer>
+              <Card.Footer.Item renderAs='a' href='/'><Level>
+                <Level.Side align='left'>
+                  <Button link>Volver</Button>
+                </Level.Side>
+               </Level></Card.Footer.Item>
+              <Card.Footer.Item renderAs='a' href='#No'>            
+              <Level>
+                <Level.Side align='left'>
+                  <Button link>comprar</Button>
+                </Level.Side>
+               </Level>
+              </Card.Footer.Item>
+              <Card.Footer.Item renderAs='a' href='#Maybe'>            
+              <Level>
+                <Level.Side align='left'>
+                  <Button link>Like</Button>
+
+                </Level.Side>
+               </Level>
+              </Card.Footer.Item>
+            </Card.Footer>
+          </Card>
+        </Columns.Column>
+
+                     </Columns>}
     </>
   )
 }
