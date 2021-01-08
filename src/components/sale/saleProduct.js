cc5368138561a74562289d6e169d0d913495c2c3
unknown <cgonzalez@iiconcepcion.edu.ar>
2021-01-08 00:57:53 -0300

    style corrections

--- src/components/sale/saleProduct.js
+++ src/components/sale/saleProduct.js
@@ -2,22 +2,27 @@ import React, { useState, useEffect } from 'react'
 import { getProductsbyID } from '../../utils'
 import Loading from '../loading/Loading'
 import { Card, Media, Image, Content, Heading, Columns, Box, Section, Level, Button } from 'react-bulma-components'
+import SaleFormModal from './saleFormModal'
 const SaleProduct = (props) => {
   console.log(props.match.params.id)
   const key = props.match.params.id
   const [producto, setProduct] = useState([])
   const [isLoading, setLoading] = useState(false)
+  const [modal, setModal]= useState(false)
   useEffect(
     () => {
       async function fetchtData () {
         const response = await getProductsbyID(key)
-        console.log(response)
         setProduct(response.data)
         setLoading(false)
       }
       fetchtData()
     }, []
   )
+  const handleClick=() => {
+    setModal(true)
+    console.log(modal);
+  }
   return (
     <>
       {isLoading && <Loading />}
@@ -28,15 +33,14 @@ const SaleProduct = (props) => {
               <Card.Header.Title>{producto.name}</Card.Header.Title>
             </Card.Header>
             <Card.Content>
-              <Media>
-                <Card.Image size='4by3' style={{ width: 300, height: 400, display: 'flex', justifyContent: 'center' }} src={producto.image_path} />
+              <Media style={{ display: 'flex', justifyContent: 'center',flexWrap:'wrap'}}>
+                <Card.Image size='4by3' style={{ width: 300, height: 400, display: 'flex', justifyContent: 'center'}} src={producto.image_path} />
                 <Media.Item>
                   <Content style={{ display: 'flex', justifyContent: 'center', alineItem: 'center' }}>
                     {producto.description}
                   </Content>
                 </Media.Item>
               </Media>
-
             </Card.Content>
             <Card.Footer>
               <Card.Footer.Item renderAs='a' href='/'><Level>
@@ -44,26 +48,27 @@ const SaleProduct = (props) => {
                   <Button link>Volver</Button>
                 </Level.Side>
                </Level></Card.Footer.Item>
-              <Card.Footer.Item renderAs='a' href='#No'>            
+              <Card.Footer.Item>            
               <Level>
                 <Level.Side align='left'>
-                  <Button link>comprar</Button>
+                  <Button onClick={handleClick}>comprar</Button>
                 </Level.Side>
                </Level>
               </Card.Footer.Item>
-              <Card.Footer.Item renderAs='a' href='#Maybe'>            
+              <Card.Footer.Item>            
               <Level>
                 <Level.Side align='left'>
-                  <Button link>Like</Button>
+                  <h3>Stock: <strong>{producto.stock}</strong></h3>
 
                 </Level.Side>
                </Level>
               </Card.Footer.Item>
             </Card.Footer>
-          </Card>
-        </Columns.Column>
+           </Card>
+         </Columns.Column>
 
-                     </Columns>}
+        </Columns>}
+      
     </>
   )
 }
