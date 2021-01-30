f858a82726c10906acfb21e9ddff581beea84f8f
unknown <cgonzalez@iiconcepcion.edu.ar>
2021-01-30 02:54:08 -0300

    update latout for conten page

--- src/components/content.js
+++ src/components/content.js
@@ -1,21 +1,24 @@
 import React from 'react'
-import Footer from './footer'
+import ContextApi from '../context/contextApi'
 import Navbar from './navbar'
 import ProductList from './product/productList'
 import LogoHome from '../components/images/logoHome.jpg'
 import { Image } from 'react-bulma-components'
+import About from './about'
+import FooterComponent from './footer'
 
 const Content = () => {
   return (
-    <div style={{}}>
-      <Navbar />
-
-      <Image size={128} alt='48x48' src={LogoHome} position='left' />
-
-      <ProductList />
-
-      <Footer />
-    </div>
+        <ContextApi.Consumer>
+         {context => (
+        <>
+        <Navbar />
+        <Image size={128} alt='48x48' src={LogoHome} position='left' />
+        {context.login ? <ProductList />: <About/>}
+        <FooterComponent />
+        </>
+      )}
+    </ContextApi.Consumer>
   )
 }
 export default Content
