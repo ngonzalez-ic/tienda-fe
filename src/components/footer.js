501cf20ffe9da15395e4c636ece38c62ff6fa7f6
unknown <cgonzalez@iiconcepcion.edu.ar>
2021-01-30 04:46:28 -0300

    new leyend footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,7 +10,7 @@ const FooterComponent = () => {
           <Container>
             <Content style={{ textAlign: 'center' }}>
               <p>
-                <strong>Bulma</strong> by <a href='http://jgthms.com'>Jeremy Thomas</a>. The source code is licensed
+                <strong>Tienda</strong> by <a href='https://github.com/ngonzalez-ic'>Carlo N Gonzalez</a>. The source code is licensed
                 <a href='http://opensource.org/licenses/mit-license.php'>MIT</a>. The website content
                 is licensed <a href='http://creativecommons.org/licenses/by-nc-sa/4.0/'>CC BY NC SA 4.0</a>.
               </p>
