9dc8806499516ee217cfd641bb9bc2e6edd10dc5
unknown <cgonzalez@iiconcepcion.edu.ar>
2021-01-30 02:57:10 -0300

    new layout

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,25 +1,40 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link,useHistory } from 'react-router-dom'
 import { Button } from 'react-bulma-components'
 import ContextApi from '../context/contextApi'
 
-const exit = ()=>{
-  localStorage.removeItem("login")
 
-}
+
+
 
 const Navigator = () => {
+  
+  const history = useHistory()
+  const exit = ()=>{
+    localStorage.removeItem("login")
+    history.push('/login')
+  }
+
+  const singIn = ()=>{
+    history.push('/login')
+  }
+
+  const singUot = ()=>{
+    history.push('/login')
+  }
+
   return (
     <ContextApi.Consumer>
       {context => (
         <div style={{ backgroundColor: '#00D1B2' }}>
           <Button color='primary' style={{ margin: '5px' }}><Link to='/' style={{ color: 'white' }}>Inicio</Link></Button>
+          
           {context.login && <Button color='primary' 
           style={{ margin: '5px' }} onClick={exit}>Salir
             </Button>}
           {!context.login && <>
-            <Button color='primary' style={{ margin: '5px' }}><Link to='/login' style={{ color: 'white' }}>Login</Link></Button>
-            <Button color='primary' style={{ margin: '5px' }}><Link to='/register' style={{ color: 'white' }}>Registrese</Link></Button>
+            <Button color='primary' style={{ margin: '5px' }} onClick={singIn}>Login</Button>
+            <Button color='primary' style={{ margin: '5px' }} onClick={singUot}>Registrese</Button>
           </>}
         </div>
       )}
